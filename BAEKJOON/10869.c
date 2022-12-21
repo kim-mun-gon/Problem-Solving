4cac6c125f199f10c7b8cc54b76fd78984cc19e7
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-21 18:02:03 +0900

    백준 10869번 브론즈 5

---
+++ BAEKJOON/10869.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    printf("%d\n", num1+num2);
+    printf("%d\n", num1-num2);
+    printf("%d\n", num1*num2);
+    printf("%d\n", num1/num2);
+    printf("%d\n", num1%num2);
+
+    return 0;
+}
