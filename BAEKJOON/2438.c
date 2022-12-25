3c0b0133e72d7c8062171efb7b7ec1448fb71ed9
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2022-12-25 17:21:45 +0900

    백준 2438번 브론즈 5

---
+++ BAEKJOON/2438.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    scanf("%d", &num);
+
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
