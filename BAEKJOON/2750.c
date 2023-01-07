974ab20da2ca884926151098d9a70123eb8673f8
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2023-01-07 15:20:33 +0900

    백준 2750번 브론즈 2

---
+++ BAEKJOON/2750.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void sort(int arr[], int n)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        printf("%d\n", arr[i]);
+}
+int main(void)
+{
+    int arr[1000];
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    sort(arr, n);
+
+    return 0;
+}
